914e9aa4ea4b5827dff842ca9f5ea59d767ca8d1
Shamaun-Nabi <snashik950@gmail.com>
2022-06-22 21:45:52 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+// import './App.css';
+function App() {
+
+  return (
+    <>
+      <h3 className="text-indigo-500">Test class</h3>
+    </>
+  );
+}
+export default App;
