6cb2b99c782d9c20c2e78350db399056aa9176d0
Shamaun-Nabi <snashik950@gmail.com>
2022-06-25 10:38:01 +0600

    post blog added

--- src/Components/BlogInfo/BlogInfo.jsx
+++ src/Components/BlogInfo/BlogInfo.jsx
@@ -4,15 +4,14 @@ import { AiFillBackward } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { BlogContext } from "../../App";
 import { motion } from "framer-motion";
+import { DiYii } from "react-icons/di";
 
 export default function BlogInfo() {
   const [shareBlog] = useContext(BlogContext);
   // no need setShareblog here
   let navigate = useNavigate();
   const { id } = useParams();
-  const blog = shareBlog.find(
-    (shareBlog) => shareBlog?._id === id
-  );
+  const blog = shareBlog.find((shareBlog) => shareBlog?._id === id);
   console.log(blog);
   // console.log(id);
 
@@ -28,11 +27,19 @@ export default function BlogInfo() {
       </div>
       <div className=" container mx-auto">
         <div className="flex justify-center py-2">
-          <motion.img initial={{ scale: 0 }} animate={{scale:1}}
+          <motion.img
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
             className=" border-8 border-slate-300 shadow-md"
             src={blog?.imageURL}
             alt=""
           />
+        </div>
+        <div>
+          <p className="text-slate-800 ">
+            <DiYii className="text-teal-600" /> {blog?.blog}
+            <DiYii className="text-teal-600" />
+          </p>
         </div>
       </div>
     </>
